2b86fdce6f309c551ec955cb341cddbd012c4ef8
Yuri Kosenko <kosurij.dm@yandex.ru>
2022-11-09 22:11:37 +0300

    Первая версия мессенджера. Реализована верстка и сборка pacel'ем и шаблонизация handlebars'ом.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static('./dist'));
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
