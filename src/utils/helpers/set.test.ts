1da7eeae20a888cdff2845c26a5a2227bcaad856
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-01-09 13:27:07 +0300

    Настроено окружение, добавлен первый тест

---
+++ src/utils/helpers/set.test.ts
@@ -0,0 +1,40 @@
+import set from "./set";
+import { expect } from "chai";
+import { TPlainObject } from "./isPlainObject";
+
+describe('set function', () => {
+  it('should return passed object if it not a plain object', () => {
+    //arrange
+    const obj: TPlainObject = [];
+
+    //act
+    const result = set(obj, 'a.b', 3)
+
+    //assert
+    expect(result).to.eq(obj)
+  });
+
+  it('should return passed null if null is passed as first argument', () => {
+    //arrange
+
+    //act
+
+    //assert
+  });
+
+  it('should set new property to passed object with passed value', () => {
+    //arrange
+
+    //act
+
+    //assert
+  });
+
+  it('should not return new object', () => {
+    //arrange
+
+    //act
+
+    //assert
+  });
+})
